6a7bd6e9f2eedbe3bbbb3af0741293c34b263ac4
beldin0 <beldin0@hotmail.com>
2018-06-14 14:48:25 +0100

    API 1.3 (config file settings for dummy db or live)

--- MembershipSystem/Startup.cs
+++ MembershipSystem/Startup.cs
@@ -21,29 +21,32 @@ namespace MembershipSystem
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add authentication 
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = CustomAuthOptions.DefaultScheme;
                 options.DefaultChallengeScheme = CustomAuthOptions.DefaultScheme;
             })
-            // Call custom authentication extension method
             .AddCustomAuth(options =>
             {
-                // Configure single or multiple passwords for authentication
                 options.AuthKey = "custom auth key";
             });
 
             services.AddMvc(options =>
             {
-                // All endpoints need authentication
                 options.Filters.Add(new AuthorizeFilter(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build()));
             });
 
-            bool UseDummyDb = Configuration.GetValue<bool>("UseDummyInMemory");
             Func<DbContextOptionsBuilder<MembersContext>, DbContextOptionsBuilder<MembersContext>> opt;
+            bool UseDummyDb = Configuration.GetValue<bool>("UseDummyInMemory");
+            if (UseDummyDb)
+            {
                 opt = o => o.UseInMemoryDatabase("dummy_database");
-                MembersController.options = opt(new DbContextOptionsBuilder<MembersContext>()).Options;
+            } else
+            {
+                opt = o => o.UseSqlServer(Configuration.GetValue<string>("DefaultConnection"));
+            }
+            DbContextOptions<MembersContext>  contextOptions = opt(new DbContextOptionsBuilder<MembersContext>()).Options;
+            MembersController.options = contextOptions;
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -52,8 +55,6 @@ namespace MembershipSystem
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            // Enable authentication capabilities
             app.UseAuthentication();
             app.UseMvc();
         }
